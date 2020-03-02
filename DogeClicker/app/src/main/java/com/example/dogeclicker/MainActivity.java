fcff15dfabfcd04222f9390ceaea2a1a368c00a3
Alex McDowell <scruffyalexhs@gmail.com>
2020-03-02 09:08:26 -0700

    models
    adding the base neede models

--- DogeClicker/app/src/main/java/com/example/dogeclicker/MainActivity.java
+++ DogeClicker/app/src/main/java/com/example/dogeclicker/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dogeclicker;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static java.lang.System.out;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
