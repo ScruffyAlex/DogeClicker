75ba111d17f2a8980a34104c6fff3e47e737c0f5
Alex McDowell <scruffyalexhs@gmail.com>
2020-03-04 08:40:03 -0700

    switch

--- DogeClicker/app/src/main/java/com/example/dogeclicker/controller/ClickManager.java
+++ DogeClicker/app/src/main/java/com/example/dogeclicker/controller/ClickManager.java
@@ -78,6 +78,52 @@ public class ClickManager {
                         break;
                     case 7:
                         break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        break;
+                    case 20:
+                        break;
+                    case 21:
+                        break;
+                    case 22:
+                        break;
+                    case 23:
+                        break;
+                    case 24:
+                        break;
+                    case 25:
+                        break;
+                    case 26:
+                        break;
+                    case 27:
+                        break;
+                    case 28:
+                        break;
+                    case 29:
+                        break;
+                    case 30:
+                        break;
                 }
 
 
