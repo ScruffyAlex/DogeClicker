15599c36e0fee45eb25cf637bbd33fbfa2f724cf
Alex McDowell <scruffyalexhs@gmail.com>
2020-03-03 19:18:07 -0700

    addition of random events
    added the beginning of the random event being run

--- DogeClicker/app/src/main/java/com/example/dogeclicker/controller/ClickManager.java
+++ DogeClicker/app/src/main/java/com/example/dogeclicker/controller/ClickManager.java
@@ -2,6 +2,8 @@ package com.example.dogeclicker.controller;
 
 import com.example.dogeclicker.models.Event;
 
+import java.util.Random;
+
 public class ClickManager {
     public float coinSum = 0;
     public int skillPointSum = 0;
@@ -32,12 +34,38 @@ public class ClickManager {
     }
 
     public boolean removeUpgrade(){
+
         return true;
     }
 
     public float runEvent(){
+        Random rng = new Random();
 
-                return 0f;
+        rng.nextInt(10);
+        int coinEffect = rng.nextInt(999999);
+        Event currentEvent= new Event("Test",0,"Test",false);
+                switch(rng.nextInt(5)) {
+                    
+                    case 1:
+
+                        currentEvent = new Event("Wild Doge!", coinEffect,"A wild doge has " +
+                                "entered your home! They left you "+coinEffect+" Doge Coins!",true);
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                }
+
+
+                return currentEvent.getEffect();
     }
 
     public float click(){
